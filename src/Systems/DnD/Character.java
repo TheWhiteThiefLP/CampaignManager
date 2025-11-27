392ead3b860f8f000cc222adff9cce8d611968bc
TheWhiteThiefLP <45312911+TheWhiteThiefLP@users.noreply.github.com>
2025-11-27 13:21:53 +0100

    Building Framework

---
+++ src/Systems/DnD/Character.java
@@ -0,0 +1,14 @@
+package Systems.DnD;
+
+import Systems.General.Ability;
+
+public class Character {
+    private String name;
+    private String playerName;
+    private int xp;
+    
+
+    private Ability[] ability;
+
+
+}
